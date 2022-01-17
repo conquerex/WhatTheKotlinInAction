99deef6cf6e4d25dcb05bebbeb75cf5585199dca
Jongkook <barley.son@Jongkooks-MacBook-Pro.local>
2022-01-17 19:33:57 +0900

    ch2.3.5

--- src/main/kotlin/ch2/Ch2.kt
+++ src/main/kotlin/ch2/Ch2.kt
@@ -1,11 +1,66 @@
 package ch2
 
+import ch2.Color.*
+
 fun main() {
-    println("===== ch2.3.1 =====")
+    println("\n\n===== ch2.3.1 =====")
     println(Color.BLUE.rgb())
     println(Color.GREEN.rgb())
     println(Color.VIOLET.rgb())
+
+    println("\n\n===== ch2.3.3 =====")
+    println(mix(BLUE, YELLOW))
+
+    println("\n\n===== ch2.3.5 =====")
+    println(eval((Sum(Sum(Num(1), Num(4)), Num(5)))))
+
+    println("\n\n===== ch ooo =====")
+
+
+    println("\n\n===== ch ooo =====")
+
+
+    println("\n\n===== ch ooo =====")
+
+
+    println("\n\n===== ch ooo =====")
+
+
 }
+
+// ========================== 2.3.5 ==========================
+interface Expr
+
+// value라는 프로퍼티만 존재하는 단순한 클래스. Expr 인터페이스를 구현
+class Num(val value: Int) : Expr
+
+// Expr 타입의 객체라면 어떤 것이나 Sum 연산의 인자가 될 수 있다
+// 따라서 Num이나 다른 Sum이 인자로 올 수 있다
+class Sum(val left: Expr, val right: Expr) : Expr
+
+fun eval(e: Expr): Int {
+    if (e is Num) {
+        val n = e as Num // 여기서 Num으로 타입을 변환하는데 이는 불필요한 중복
+        return n.value
+    }
+    if (e is Sum) {
+        return eval(e.right) + eval(e.left) // 변수 e에 대해 스마트 캐스트를 사용
+    }
+    throw IllegalArgumentException("Unknown expression")
+}
+
+
+// ========================== 2.3.3 ==========================
+
+fun mix(c1: Color, c2: Color) =
+    when (setOf(c1, c2)) {
+        // when 식의 인자로 아무 객체나 사용할 수 있다
+        // when은 이렇게 인자로 받은 객체가 각 분기 조건에 있는 객체와 같은지 테스트한다.
+        setOf(RED, YELLOW) -> ORANGE // 두 색을 혼합해서 다른 색을 만들수 있는 경우를 열거
+        setOf(YELLOW, BLUE) -> GREEN
+        setOf(BLUE, VIOLET) -> INDIGO
+        else -> throw Exception("Dirty color") // 매치되는 분기 조건이 없으면 이 문장을 실행
+    }
 
 enum class Color(
     val r: Int, val g: Int, val b: Int //상수의 프로퍼티 정의
